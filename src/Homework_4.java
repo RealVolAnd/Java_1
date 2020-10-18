17565defd064a365f581d550297de9b68c84e5e0
RealVolAnd <out_of_bound@yahoo.com>
2020-10-18 10:56:14 +0300

    Полностью готовый релиз.

--- src/Homework_4.java
+++ src/Homework_4.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Homework_4 {
 
+
     private static final String RESET = "\u001B[0m";
     private static final String BLACK = "\u001B[30m";
     private static final String RED = "\u001B[31m";
@@ -20,7 +21,7 @@ public class Homework_4 {
     private static final int ALIGN_LEFT=1;
     private static final int ALIGN_CENTER=0;
 
-    private static final int MAPSIZE = 3;
+    private static final int MAPSIZE = 5;//!!!!!!!!!!!!!!!!!!!! Это размер поля !!!!!!!!!!!!!!!!!!!
     private static final char DOT_EMPTY = '•';
     private static final char DOT_X = 'X';
     private static final char DOT_O = 'O';
@@ -34,12 +35,20 @@ public class Homework_4 {
 
     private static int DOTS_TO_WIN;
     private static Scanner scan = new Scanner(System.in);
-    private static Random rand = new Random();
     private static int x=0,y=0;
     private static int diaganalCount=0;
 
-    public static void main(String[] args) throws Exception {
 
+    /*
+    Приняты следующие условия и ограничения:
+    1. Размер поля всегда нечетный, начиная с 3х3.
+    2. для поля 3х3 выигрышная комбинация состоит из 3 симвошов, для прочих полей - из 4.
+    3. вводить координаты нужно последовательно - сначала строку, затем столбец.
+     */
+
+
+    public static void main(String[] args) throws Exception {
+    //===============================================================  START =======================
         initTheGame();
         drawHeader();
 
@@ -49,7 +58,7 @@ public class Homework_4 {
             }
     }
 
-
+    //--------------------------------------------  GAME MAIN CYCLE  ---------------------------------
     private static void startTheGame(){
         String xy="";
         int result=99;
@@ -71,32 +80,193 @@ public class Homework_4 {
 
 
     //------------------------------------------ AI TURN ----------------------------------
+    /*
+    Алгоритм решения:
+    1.Проверка наличия выигрышной комбинации
+    2.Обязательная блокировка противника.
+    3.Выстраивание линии
+    4.Случайный ход
+     */
     private static void aiTurn(){
-        int turn;
         refreshVectorBig();
 
 // 1.Проверка наличия выигрышной комбинации
 
+        int isWin=-1;
+        String tmpTemplate="";
+        for (int i = 0; i < vectorBig.length; i++) {
+            if(DOTS_TO_WIN==3){
+                if(vectorBig[i].contains("OO"+DOT_EMPTY))isWin=i;
+                if(vectorBig[i].contains("O"+DOT_EMPTY+"O"))isWin=i;
+                if(vectorBig[i].contains(DOT_EMPTY+"OO"))isWin=i;
+            } else {
+                if(vectorBig[i].contains("OOO"+DOT_EMPTY))isWin=i;
+                if(vectorBig[i].contains("O"+DOT_EMPTY+"OO"))isWin=i;
+                if(vectorBig[i].contains("OO"+DOT_EMPTY+"O"))isWin=i;
+                if(vectorBig[i].contains(DOT_EMPTY+"OOO"))isWin=i;
+            }
+        }
+
+    if(isWin!=-1){
+        int tmpIdx=-1;
+        String tmpString=vectorBig[isWin];
+
+        if(DOTS_TO_WIN==3) {
+            if (tmpString.contains("OO" + DOT_EMPTY)) {
+                tmpIdx = tmpString.indexOf("OO" + DOT_EMPTY)+2;
+            } else if(tmpString.contains(DOT_EMPTY+"OO")) {
+                tmpIdx = tmpString.indexOf(DOT_EMPTY+"OO");
+            }else{
+                tmpIdx = tmpString.indexOf("O"+DOT_EMPTY+"O")+1;
+            }
+
+        }else{
+
+            if (tmpString.contains("OOO" + DOT_EMPTY)) {
+                tmpIdx = tmpString.indexOf("OOO" + DOT_EMPTY)+3;
+            } else if(tmpString.contains(DOT_EMPTY+"OOO")){
+                tmpIdx = tmpString.indexOf(DOT_EMPTY+"OOO");
+            }else if(tmpString.contains("O"+DOT_EMPTY+"OO")){
+                tmpIdx = tmpString.indexOf("O"+DOT_EMPTY+"OO")+1;
+            }else{
+                tmpIdx = tmpString.indexOf("OO"+DOT_EMPTY+"O")+2;
+            }
+        }
+        putAxTurnToMap(vectorBigIndex[isWin],tmpIdx);
+        return;
+        }
 
 
+// 1.Обязательная блокировка противника.
 
-// 1.Обязательная блокировка противника. От стеночки.От нолика
+        if(DOTS_TO_WIN==3) {
+            int isHumanWin=-1;
+            int tmpIdx=-1;
+
+
+            for (int i = 0; i < vectorBig.length; i++) {
+                if(vectorBig[i].contains("XX"+DOT_EMPTY))isHumanWin=i;
+                if(vectorBig[i].contains("X"+DOT_EMPTY+"X"))isHumanWin=i;
+                if(vectorBig[i].contains(DOT_EMPTY+"XX"))isHumanWin=i;
+            }
+            if(isHumanWin>-1){
+                String tmpString=vectorBig[isHumanWin];
+                if (tmpString.contains("XX" + DOT_EMPTY)) {
+                    tmpIdx = tmpString.indexOf("XX" + DOT_EMPTY)+2;
+                } else if(tmpString.contains(DOT_EMPTY+"XX")) {
+                    tmpIdx = tmpString.indexOf(DOT_EMPTY+"XX");
+                }else{
+                    tmpIdx = tmpString.indexOf(DOT_X+DOT_EMPTY+"X")+1;
+                }
+                putAxTurnToMap(vectorBigIndex[isHumanWin],tmpIdx);
+                return;
+            }
+
+        } else {
+            int isHumanWin=-1;
+            int tmpIdx=-1;
+
+            for (int i = 0; i < vectorBig.length; i++) {
+                if(vectorBig[i].contains("XXX"+DOT_EMPTY))isHumanWin=i;
+                if(vectorBig[i].contains("X"+DOT_EMPTY+"XX"))isHumanWin=i;
+                if(vectorBig[i].contains("XX"+DOT_EMPTY+"X"))isHumanWin=i;
+                if(vectorBig[i].contains(DOT_EMPTY+"XXX"))isHumanWin=i;
+                if(vectorBig[i].contains(DOT_EMPTY+"XX"+DOT_EMPTY+DOT_EMPTY))isHumanWin=i;
+                if(vectorBig[i].contains(DOT_EMPTY+DOT_EMPTY+"XX"+DOT_EMPTY))isHumanWin=i;
+            }
+            if(isHumanWin>-1){
+                String tmpString=vectorBig[isHumanWin];
+
+           if(tmpString.contains(DOT_EMPTY+"XX"+DOT_EMPTY+DOT_EMPTY)){
+                tmpIdx = tmpString.indexOf(DOT_EMPTY+"XX"+DOT_EMPTY+DOT_EMPTY)+3;
+            } else if(tmpString.contains(DOT_EMPTY+DOT_EMPTY+"XX"+DOT_EMPTY)) {
+               tmpIdx = tmpString.indexOf(DOT_EMPTY+DOT_EMPTY+"XX"+DOT_EMPTY)+1;
+           }else if (tmpString.contains("XXX" + DOT_EMPTY)) {
+                    tmpIdx = tmpString.indexOf("XXX" + DOT_EMPTY)+3;
+                } else if(tmpString.contains(DOT_EMPTY+"XXX")) {
+                    tmpIdx = tmpString.indexOf(DOT_EMPTY+"XXX");
+                }else if(tmpString.contains("XX"+DOT_EMPTY+"X")) {
+                    tmpIdx = tmpString.indexOf("XX"+DOT_EMPTY+"X")+2;
+                } else if(tmpString.contains("X"+DOT_EMPTY+"XX")) {
+                    tmpIdx = tmpString.indexOf("X" + DOT_EMPTY + "XX") + 1;
+                }
+                putAxTurnToMap(vectorBigIndex[isHumanWin],tmpIdx);
+                return;
+            }
+        }
+
 // 1.Выстраивание линии
+
+        if(DOTS_TO_WIN==3) {
+            int isLine=-1;
+            int tmpIdx=-1;
+
+            for (int i = 0; i < vectorBig.length; i++) {
+                if(vectorBig[i].contains("O"+DOT_EMPTY))isLine=i;
+                if(vectorBig[i].contains(DOT_EMPTY+"O"))isLine=i;
+            }
+            if(isLine>-1){
+                String tmpString=vectorBig[isLine];
+                if (tmpString.contains("O" + DOT_EMPTY)) {
+                    tmpIdx = tmpString.indexOf("O" + DOT_EMPTY)+1;
+                } else if(tmpString.contains(DOT_EMPTY+"O")) {
+                    tmpIdx = tmpString.indexOf(DOT_EMPTY + "O");
+                }
+                putAxTurnToMap(vectorBigIndex[isLine],tmpIdx);
+                return;
+            }
+
+        } else {
+            int isLine=-1;
+            int tmpIdx=-1;
+
+            for (int i = 0; i < vectorBig.length; i++) {
+
+                if(vectorBig[i].contains(DOT_EMPTY+DOT_EMPTY+DOT_EMPTY+"O"))isLine=i;
+                if(vectorBig[i].contains("O"+DOT_EMPTY+DOT_EMPTY+DOT_EMPTY))isLine=i;
+                if(vectorBig[i].contains(DOT_EMPTY+"OO"))isLine=i;
+                if(vectorBig[i].contains("OO"+DOT_EMPTY))isLine=i;
+                if(vectorBig[i].contains(DOT_EMPTY+"OO" + DOT_EMPTY))isLine=i;
+                if(vectorBig[i].contains(DOT_EMPTY+"OO" + DOT_EMPTY+DOT_EMPTY))isLine=i;
+                if(vectorBig[i].contains(DOT_EMPTY+DOT_EMPTY+"OO"+DOT_EMPTY))isLine=i;
+            }
+            if(isLine>-1){
+                String tmpString=vectorBig[isLine];
+                if (tmpString.contains(DOT_EMPTY+DOT_EMPTY+DOT_EMPTY+"O")) {
+                    tmpIdx = tmpString.indexOf(DOT_EMPTY+DOT_EMPTY+DOT_EMPTY+"O")+2;
+                } else if (tmpString.contains("O"+DOT_EMPTY+DOT_EMPTY+DOT_EMPTY)) {
+                    tmpIdx = tmpString.indexOf("O"+DOT_EMPTY+DOT_EMPTY+DOT_EMPTY)+1;
+                }else if (tmpString.contains(DOT_EMPTY+"OO")) {
+                    tmpIdx = tmpString.indexOf(DOT_EMPTY+"OO");
+                } else if(tmpString.contains("OO"+DOT_EMPTY)) {
+                    tmpIdx = tmpString.indexOf("OO"+DOT_EMPTY)+2;
+                }else if(tmpString.contains(DOT_EMPTY+"OO" + DOT_EMPTY)) {
+                    tmpIdx = tmpString.indexOf(DOT_EMPTY+"OO" + DOT_EMPTY);
+                }else if(tmpString.contains(DOT_EMPTY+"OO" + DOT_EMPTY+DOT_EMPTY)) {
+                    tmpIdx = tmpString.indexOf(DOT_EMPTY+"OO" + DOT_EMPTY+DOT_EMPTY)+3;
+                }else if(tmpString.contains(DOT_EMPTY+DOT_EMPTY+"OO"+DOT_EMPTY)) {
+                    tmpIdx = tmpString.indexOf(DOT_EMPTY+DOT_EMPTY+"OO"+DOT_EMPTY)+1;
+                }
+                putAxTurnToMap(vectorBigIndex[isLine],tmpIdx);
+                return;
+            }
+        }
+
 // 1.Случайный ход
 
-        int rndVector=(int)(Math.random()*vectorBig.length);
+        int rndVector=(int)(Math.random()*vectorBig.length-1);
         while(vectorBig[rndVector].indexOf(DOT_EMPTY)<0)  {
-            rndVector=(int)(Math.random()*vectorBig.length);
+            rndVector=(int)(Math.random()*vectorBig.length-1);
         }
-        int rndIndex=(int)(Math.random()*MAPSIZE);
+        int rndIndex=(int)(Math.random()*vectorBig[rndVector].length()-1);
         while(vectorBig[rndVector].charAt(rndIndex)!=DOT_EMPTY){
-            rndIndex=(int)(Math.random()*MAPSIZE);
+            rndIndex=(int)(Math.random()*vectorBig[rndVector].length()-1);
         }
         putAxTurnToMap(vectorBigIndex[rndVector],rndIndex);
 
-        //checkWinner();
-    }
 
+    }
+//----------------------------------------- PUT AI TURN TO THE MAP --------------------------
     private static void putAxTurnToMap(String vector,int idx){
         int aiX=0,aiY=0;
         String[] s=vector.split(" ");
@@ -134,16 +304,11 @@ public class Homework_4 {
             }
             x--;
             y--;
-        map[x][y]='X';
-       // checkWinner();
+        if(map[x][y]==DOT_EMPTY)map[x][y]='X';
+
     }
 
-
-
-
-
-
-//-------------------------------------------- CHECK WINNER --------------------------------
+//-------------------------------------------- CHECK THE WINNER --------------------------------
 
     private static int checkWinner(){
         int res=0;
@@ -170,7 +335,7 @@ public class Homework_4 {
         return res;
     }
 
-
+    //------------------------------------------  BIG VECTOR RESET  -----------------------------------
     private static void vectorBigReset(){
         for (int i = 0; i <vectorBig.length ; i++) vectorBig[i]="";
     }
@@ -266,12 +431,11 @@ public class Homework_4 {
             cntr++;
             maxValue++;
         }
-
-
     }
-
-
-
+    //------------------------------------------  SMALL VECTOR RESET  -----------------------------------
+    private static void vectorSmallReset(){
+        for (int i = 0; i <vectorSmall.length ; i++) vectorSmall[i]="";
+    }
 //--------------------------------------------- REFRESH THE SMALL VECTOR ------------------------------
     private static void refreshVectorSmall(){
         int tmpX,tmpY;
@@ -282,6 +446,7 @@ public class Homework_4 {
         l-левая диагональ
         r-правая диагональ
          */
+        vectorSmallReset();
         clearSmallVectorIndex();
         if(DOTS_TO_WIN>0){
 
@@ -305,8 +470,6 @@ public class Homework_4 {
             for (int i = 0; i <MAPSIZE ; i++) {
                 vectorSmallIndex[0]+=x+","+i+" ";
             }
-
-
 
             // заполняем вертикаль
             tmpX=x;
@@ -411,7 +574,7 @@ private static void clearBigVectorIndex(){
         }
     }
 
-
+    //----------------------------------------------- SHOW MAP -----------------------------------
 
     private static void showMap(char[][] arr) {
 
@@ -425,15 +588,23 @@ private static void clearBigVectorIndex(){
         println("\t\t\t\t  "+RED+BOLD+"X"+REGUL);
 
         for (int i = 0; i < arr.length; i++) {
-            System.out.print("\t\t\t\t  "+RED+(i+1)+RESET+"\t");
+
+                System.out.print("\t\t\t\t  "+RED+(i+1)+RESET+"\t");
+
+
             for (int j = 0; j < arr[i].length; j++) {
-                System.out.print("\t"+arr[i][j]);
+                if(i<9){
+                    System.out.print("\t"+arr[i][j]);
+                }else{
+                    System.out.print(arr[i][j]+"\t");
+                }
+
             }
             System.out.println();
         }
         println("\t\t\t\t");
     }
-
+    //-----------------------------------------  INIT THE GAME ------------------------------------
     private static void initTheGame(){
         int tmpVar=0;
         if((MAPSIZE<3)||(MAPSIZE%2==0)){
@@ -444,6 +615,7 @@ private static void clearBigVectorIndex(){
         }
         if(MAPSIZE==3) DOTS_TO_WIN=3;
         if(MAPSIZE>3) DOTS_TO_WIN=4;
+
         map=new char[MAPSIZE][MAPSIZE];
         vectorSmall=new String[4];
         vectorSmallIndex=new String[4];
@@ -460,7 +632,7 @@ private static void clearBigVectorIndex(){
          vectorBigIndex=new String[tmpVar];
     }
 
-
+    //----------------------------------------- CHECK RESTART ----------------------------
     private static boolean checkRestart(){
         boolean b=false;
         int result=100;
@@ -478,7 +650,7 @@ private static void clearBigVectorIndex(){
         return b;
     }
 
-
+    //---------------------------------------- SHOW END MESSAGE  -----------------------------------
     private static void showEndMessage(int whoWin){
 
         print(RESET);
@@ -506,7 +678,7 @@ private static void clearBigVectorIndex(){
     }
 
 
-
+    //------------------------------------ SHOW HEADER -----------------------------------------
     private static void drawHeader(){
         print("\u2554");
         drawHLine2(30);
@@ -526,7 +698,7 @@ private static void clearBigVectorIndex(){
         drawHLine2(3);
         println("\u255d");
     }
-
+    //---------------------------------------  A FEW GRAPHIC FUNCTIONS --------------------
     private static void print(String str){
         System.out.print(str);
     }
